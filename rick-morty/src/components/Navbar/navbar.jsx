967d2b9f7bc1bf946eb82d5a9ab5f36f41562d14
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-10-17 14:56:46 -0500

    update

--- rick-morty/src/components/Navbar/navbar.jsx
+++ rick-morty/src/components/Navbar/navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = ({ onSearch }) => { // Se recibe onSearch como prop
   return (
     <div className='head-container'>
       <div className='navbar-container'>
+        
         <div className='logo'>
           <img src={logo} alt="logo" />
         </div>
@@ -20,6 +21,7 @@ const Navbar = ({ onSearch }) => { // Se recibe onSearch como prop
         <div>
           <Search onSearch={onSearch} /> {/* Pasa la prop onSearch a Search */}
         </div>
+
       </div>
       <div className='linear-gradient'>
       </div>
